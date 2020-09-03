c1f28c55cad62e8b7ebaab62d20f15f3f2bc4e1c
Uchoosecode <awittyl44@gmail.com>
2020-09-02 22:25:17 -0400

    created scale for the cells in the canvas

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", init)
 
 const canvas = document.getElementById("canvas");
-const context = canvas.getContext("2D");
+const context = canvas.getContext("2d");
 const size = 800;
+const scale = 8;
 
 function init() {
     canvas.width = size;
     canvas.height = size;
+    context.scale(scale, scale);
+    context.fillStyle = "black";
 }
